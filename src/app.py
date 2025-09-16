ccd9734601aa1e09b4c1a8fc19b2fb3803d11456
Ajeethkumar07 <ajeethk67@gmail.com>
2025-09-16 11:36:11 +0530

    Updated the names

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ load_dotenv()
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-local_css("style.css")
+local_css("design.css")
 
 # Hide Streamlit menu and footer globally
 hide_streamlit_style = """
